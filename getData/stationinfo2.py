e7cf90aa2f0c98931e0ad9949d8c355137ec52ca
wslukaa <lukwingsan@Luks-MacBook-Pro.local>
2017-09-12 10:20:51 +0800

    add

---
+++ getData/stationinfo2.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import urllib
+import time, sys, MySQLdb
+
+cities = ["beijing", "shanghai", "yinchuan", "wulumuqi", "haerbin", "haikou", "guangzhou"]
+cities_name = ["北京", "上海", "银川", "乌鲁木齐", "哈尔滨", "海口", "广州"]
+urladr = "http://www.pm25s.com/"
+TIMEFORMAT = '%Y-%m-%d %X'
+
+def main():
+
+	ti = time.strftime(TIMEFORMAT, time.localtime(time.time()))
+
+	conn = MySQLdb.connect(
+		host = 'localhost',
+		user = 'root',
+		passwd = 'snowdrop',
+		db = 'sdweather',
+		charset = 'utf8'
+		)
+	cur = conn.cursor()
+	iid = "insert into station (area, name) values (%s, %s)"
+	f = file("text.txt", "w")
+	for i in range(0, len(cities)):
+		f.write(cities_name[i]+':\n')
+		area = cities_name[i]
+		addr = urladr + cities[i] + ".html"
+		html = urllib.urlopen(addr).read()
+		reg = r'class="site">([\x80-\xff]+)</span><span class="\w+">\d+</span><span class="aqis">(\d+)</span>'
+		pattern = re.compile(reg)
+		lt = pattern.findall(html)
+		for r in lt:
+			values = []
+			values.append(area)
+			values.append(r[0])
+			cur.execute(iid, values)
+		conn.commit()
+	cur.close()
+	conn.close()
+	print "Successed"
+
+if __name__ == '__main__':
+	main()
